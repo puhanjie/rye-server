df9e889725a87a3895907c08b5eba01d4bafef8d
puhanjie <hanjie.pu@outlook.com>
2023-08-12 21:54:32 +0800

    配置文件数据库密码加密

--- src/test/java/com/puhj/rye/RyeApplicationTests.java
+++ src/test/java/com/puhj/rye/RyeApplicationTests.java
@@ -1,5 +1,6 @@
 package com.puhj.rye;
 
+import com.alibaba.druid.filter.config.ConfigTools;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,16 @@ class RyeApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    /**
+     * 生成数据库用户加密密码和公钥
+     */
+    @Test
+    void encrypt() throws Exception {
+        // 数据库明文密码
+        String password = "root123456";
+        ConfigTools.main(new String[]{password});
     }
 
 }
